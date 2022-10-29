9067f53b75c7279930a1d34415040e1d8805bc63
nickguerra <nickg@maliciousgames.com>
2022-10-29 05:27:31 -0500

    Added notes on regex statements

--- RegexPractice/Program.cs
+++ RegexPractice/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * ^ - Starts with
+ * $ - Ends with
+ * [] - Range
+ * () - Group
+ * . - Single character once
+ * + - one or more characters in a row
+ * ? - optional preceding character match
+ * \ - escape character
+ * \n - New line
+ * \d - Digit
+ * \D - non-digit
+ * \s - White space
+ * \S - non-white space
+ * \w - alphanumeric/underscore character (word chars)
+ * \W - non-word characters
+ * {x,y} - Repeat low (x) to high (y) (no "y" means at least x, no ",y" means
+ * {x|y} - Alternative - x or y
+ * 
+ * [^x] - Anything but x (where x is whatever character you want)
+*/
+
+using System.Text.RegularExpressions;
+
+string pattern = "";
+string toSearch = "";
