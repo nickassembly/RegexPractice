5f95635c66576bfb166d48266be75518bdd067de
nickguerra <nickg@maliciousgames.com>
2022-10-30 04:56:46 -0500

    Added regex patterns to find name in various ways

--- RegexPractice/Program.cs
+++ RegexPractice/Program.cs
@@ -14,13 +14,33 @@
  * \S - non-white space
  * \w - alphanumeric/underscore character (word chars)
  * \W - non-word characters
- * {x,y} - Repeat low (x) to high (y) (no "y" means at least x, no ",y" means
+ * {x,y} - Repeat low (x) to high (y) (no "y" means at least x, no ",y" means that many)
  * {x|y} - Alternative - x or y
  * 
  * [^x] - Anything but x (where x is whatever character you want)
 */
 
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
-string pattern = "";
-string toSearch = "";
+string pattern = @"(\s|^)Tim(\s|$)";
+
+Console.WriteLine("Tim Corey: " + Regex.IsMatch("Tim Corey", pattern));
+Console.WriteLine("Timothy Corey: " + Regex.IsMatch("Timothy Corey", pattern));
+Console.WriteLine("Always Tim: " + Regex.IsMatch("Always Tim", pattern));
+Console.WriteLine("I Am Tim Corey: " + Regex.IsMatch("I Am Tim Corey", pattern));
+
+//Stopwatch stopwatch = new();
+
+//stopwatch.Start();
+//Regex test = new Regex(pattern);
+
+//for (int i = 0; i < 100000; i++)
+//{
+//    //Regex.IsMatch("I Am Tim Corey", pattern);
+//    test.IsMatch("I Am Tim Corey");
+//}
+
+//stopwatch.Stop();
+
+//Console.WriteLine($"Time Elapsed in ms: {stopwatch.ElapsedMilliseconds}");
